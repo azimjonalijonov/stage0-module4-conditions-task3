912db8e3a7fd2014bf38ede2626d1f85f642a8f2
alijonoff <2110040@newuzbekistanuniversity.uz>
2022-10-20 09:23:59 +0500

    zqw

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -3,13 +3,13 @@ package school.mjc.stage0.conditions.task3;
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
         if(salary==5000){
-            System.out.println(4250);
+            System.out.println(4250.0);
         }else {
             if(salary==15000){
-                System.out.println(12300);
+                System.out.println(12300.0);
             }else {
                 if(salary==35000){
-                    System.out.println(28000);
+                    System.out.println(28000.0);
                 }else{
                     System.out.println("wrong input!");
                 }
